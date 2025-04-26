66712fd3b07cd59f34422c256a27d1687a3a8fd7
Omniladder <dustintobrien@gmail.com>
2025-04-26 15:10:01 -0400

    Home Screen Front-end built

--- app/src/main/java/com/hfad/flashcardapp/MainActivity.java
+++ app/src/main/java/com/hfad/flashcardapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hfad.flashcardapp;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +22,38 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            this.build_buttons();
             return insets;
         });
+    }
+
+    private void set_flashcard_settings(Button new_button){
+        new_button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+        new_button.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 205, getResources().getDisplayMetrics())
+        );
+        params.setMargins(
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()),
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()),
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()),
+                0
+        );
+        new_button.setLayoutParams(params);
+    }
+    private Button create_flashcard_button(String subject) {
+        Button new_button = new Button(this);
+        new_button.setText(subject);
+        set_flashcard_settings(new_button);
+        return new_button;
+    }
+    private void build_buttons(){
+        String[] card_subjects = getResources().getStringArray(R.array.flashcard_subjects);
+        LinearLayout card_segment = findViewById(R.id.flashcard_buttons);
+        for(String subject : card_subjects) {
+            Button new_button = this.create_flashcard_button(subject);
+            card_segment.addView(new_button);
+        }
     }
 }
