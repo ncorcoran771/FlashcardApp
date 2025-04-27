991b15a5fddd03337038e06581b9fdf6c12336a7
Omniladder <dustintobrien@gmail.com>
2025-04-26 23:12:43 -0400

    Flipping Animation

--- app/src/main/java/com/hfad/flashcardapp/Study_Screen.java
+++ app/src/main/java/com/hfad/flashcardapp/Study_Screen.java
@@ -1,8 +1,11 @@
 package com.hfad.flashcardapp;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,6 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class Study_Screen extends AppCompatActivity {
     int card_index = 0;
     int deck_size;
+    boolean isFlipped = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +28,35 @@ public class Study_Screen extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            RelativeLayout flash_card = findViewById(R.id.flashcard);
+            flash_card.setOnClickListener(new View.OnClickListener() {
+                boolean isFlipped = false;
+
+                @Override
+                public void onClick(View v) {
+                    TextView questionText = findViewById(R.id.question_text);
+                    RelativeLayout flash_card = findViewById(R.id.flashcard);
+                    flash_card.setRotationY(flash_card.getRotationY() + 180);
+                    flash_card.animate().rotationYBy(180);
+
+                    if (isFlipped) {
+                        questionText.setText("What is a Semaphore?");
+
+                    } else {
+                        questionText.setText("A semaphore controls access to a shared resource.");
+                    }
+
+                    isFlipped = !isFlipped;
+                    };
+
+            });
+
             updateView();
             return insets;
         });
     }
+
 
     private void _update_question() {
         String[] string_array = getResources().getStringArray(R.array.question_list);
@@ -45,6 +74,7 @@ public class Study_Screen extends AppCompatActivity {
     }
     public void next_card(View view){
         card_index = (card_index + 1) % deck_size;
+        isFlipped = false;
         updateView();
     }
 
@@ -53,6 +83,7 @@ public class Study_Screen extends AppCompatActivity {
         if(card_index < 0){
             card_index = deck_size - 1;
         }
+        isFlipped = false;
         updateView();
     }
 
