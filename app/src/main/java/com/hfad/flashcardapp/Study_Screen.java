4001d89745eda3e728d88d96426e8620493e32b8
Omniladder <dustintobrien@gmail.com>
2025-04-26 20:33:18 -0400

    Imma Save this and Play some video games till I get bored and come back

--- app/src/main/java/com/hfad/flashcardapp/Study_Screen.java
+++ app/src/main/java/com/hfad/flashcardapp/Study_Screen.java
@@ -3,6 +3,7 @@ package com.hfad.flashcardapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Study_Screen extends AppCompatActivity {
-
+    int card_index = 0;
+    int deck_size = 25;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +22,34 @@ public class Study_Screen extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            updateView();
             return insets;
         });
     }
 
+    public void next_card(View view){
+        card_index = (card_index + 1) % deck_size;
+        updateView();
+    }
+
+    public void prev_card(View view){
+        card_index = (card_index - 1);
+        if(card_index < 0){
+            card_index = deck_size - 1;
+        }
+        updateView();
+    }
+
+
+
     public void goToMenu(View view){
         Intent intent = new Intent(Study_Screen.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void updateView(){
+        TextView index_view = findViewById(R.id.card_indexer);
+        String index_string = (card_index + 1) + " / " + deck_size;
+        index_view.setText(index_string);
     }
 }
